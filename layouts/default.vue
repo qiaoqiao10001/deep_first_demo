1ccea8d7c5642a0189a0f75dfd4847a910d69918
zhangqiao <qiaoqiao10001@gmail.com>
2019-02-15 17:08:58 +0800

    动态路由

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nuxt-link to="/">首页</nuxt-link>
+    <nuxt-link to="/home">home</nuxt-link>
+    <nuxt-link to="/order">orders</nuxt-link>
+    <nuxt-link to="/goods_list">goodslist</nuxt-link>
+
+    <nuxt />
+
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ pages/goods_list/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h2>goods_list商品列表</h2>
+    <ul>
+        <li :key="item.name" v-for="item of list">
+            <nuxt-link :to="'/detail/' + item.id" >{{item.name}}</nuxt-link>
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            list:[
+                {id:'001',name:'iponexxx',price:2000},
+                {id:'002',name:'mixx',price:4000},
+                {id:'003',name:'huaweixxx',price:1000}
+            ]
+        }
+    }
+}
+</script>
